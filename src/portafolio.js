39a3d8865e48458ad2dc1da8bf9a59511aea864d
Pablo Benedix <pabling.benedix@gmail.com>
2021-09-20 10:59:35 -0400

    Proyecto Clon de Google Agregado

--- src/portafolio.js
+++ src/portafolio.js
@@ -5,44 +5,44 @@ import './portafolio.css';
 var articlePortafolioList = [
     {
         id: 1,
-        project_title: 'Benedix Automotores', 
-        project_name: 'HTML y CSS', 
-        project_date: '07/04/2019', 
-        project_url: 'https://zacksten.github.io/Automors-Benedix/', 
-        project_description: 'Página donde se puede apreciar los vehículos en venta de Benedix Automores.', 
-        project_image: 'https://elloplur.sirv.com/imagesPortfolio/benedix-automores.png', 
-        project_image_descripion: 'proyecto del curso de GitHub'
-    },
-    {
-        id: 2,
         project_title: 'Instagram-Model', 
         project_name: 'CSS Grid Layout', 
         project_date: '03/05/2019', 
-        project_url: 'https://zacksten.github.io/Instragram-model/', 
+        project_url: 'https://peblo-code.github.io/Instragram-model/', 
         project_description: 'Diseño web de Instagram.', 
         project_image: 'https://elloplur.sirv.com/imagesPortfolio/instagram-project.png', 
         project_image_descripion: 'proyecto del curso de CSS Grid Layout'
     },
     {
-        id: 3,
+        id: 2,
         project_title: 'Pinterest-Model', 
         project_name: 'CSS Grid Layout', 
         project_date: '03/05/2019', 
-        project_url: 'https://zacksten.github.io/Pinterest-model/', 
+        project_url: 'https://peblo-code.github.io/Pinterest-model/', 
         project_description: 'Diseño web de Pinterest.', 
         project_image: 'https://elloplur.sirv.com/imagesPortfolio/pinterest-project.png', 
         project_image_descripion: 'proyecto del curso de CSS Grid Layout'
     },
     {
-        id: 4,
+        id: 3,
         project_title: 'Reflect', 
         project_name: 'Open Source Template', 
         project_date: '29/07/2019', 
-        project_url: 'https://zacksten.github.io/Reflect/', 
+        project_url: 'https://peblo-code.github.io/Reflect/', 
         project_description: 'Este es un template de código abierto diseñado para cualquier tipo de uso.', 
         project_image: 'https://elloplur.sirv.com/imagesPortfolio/reflect.png', 
         project_image_descripion: 'Layout Open Source'
     },
+    {
+        id: 4,
+        project_title: 'Clon de Google',
+        project_name: 'Autodidact Project',
+        project_date: '08/2020',
+        project_url: 'https://peblo-code.github.io/Clon-de-Google/',
+        project_description: 'Utilizo mis conocimientos en CSS para intentar clonar Google.',
+        project_image: 'https://elloplur.sirv.com/imagesPortfolio/imagen_2021-09-20_105758.png',
+        project_image_descripion: 'Google Clon Image Project'
+    }
 ]
 
 function Portafolio(props) {
